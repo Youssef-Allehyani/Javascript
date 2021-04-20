50157e2d1493e4d214bc3d196fcb28f04dcb3445
Allehyani-Youssef <yousef4912@gmail.com>
2021-04-20 10:42:13 +0300

    Variables and  Constants

---
+++ Basic_of_Javascript/Variables_and_Constants.js
@@ -0,0 +1,31 @@
+
+// circle area 
+const Pi = 3.14 ;
+var Redius = 16;
+
+{
+    let redius = 12 ; 
+    let CircleAre = Pi * redius**2 ; 
+    document.getElementById("CircleArea1").innerHTML = `The circel area of ${redius} is ${CircleAre}`;
+}
+
+var CircleAre = Pi * Redius**2;
+document.getElementById("CircleArea2").innerHTML =`The circel area of ${Redius} is ${CircleAre}`;
+
+
+
+
+// 
+// const Pi = 3.14;
+// var Radius = 16
+// {
+// let Pi = 6;
+// let radius= 12;
+// let Result = Pi*radius**2 ;
+
+// console.log(Result);
+// document.getElementById("Result").innerHTML = `The circle area of ${radius}  redius is: ${Result}`;
+// }
+// const Result = Pi*Radius**2;
+
+// document.getElementById("Result2").innerHTML = `The circle area of ${Radius}  redius is: ${Result}`;

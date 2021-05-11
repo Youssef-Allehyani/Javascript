d60418b0dac35a6b254f1e2e5a2579646ce057c1
Allehyani-Youssef <yousef4912@gmail.com>
2021-05-11 09:48:03 +0300

    WebStorgeAPI

---
+++ Basic_of_Javascript/WebStorageAPI.js
@@ -0,0 +1,30 @@
+//web Storage API
+
+
+const Array =["code","code","code"];
+const theObject = {
+    name :"youssef",
+    hobbies : ["eat","Sleep","code"],
+    logName : function (){
+        console.log(this.name);
+    } 
+};
+
+sessionStorage.setItem("theSessionStorag",JSON.stringify(theObject));
+const mySessionData1 = sessionStorage.getItem("theSessionStorag");
+const mySessionData2 = JSON.parse(sessionStorage.getItem("theSessionStorag"));
+console.log(mySessionData1)
+console.log(mySessionData2)
+
+localStorage.setItem("theArraySession",JSON.stringify(Array));
+// localStorage.removeItem("theArraySession")
+// localStorage.clear()
+const key =localStorage.key(0)
+
+
+const ArraySession = localStorage.getItem("theArraySession");
+const ArraySession2 = JSON.parse(localStorage.getItem("theArraySession"));
+console.log(ArraySession)
+console.log(ArraySession2)
+console.log(key)
+console.log(localStorage.length)

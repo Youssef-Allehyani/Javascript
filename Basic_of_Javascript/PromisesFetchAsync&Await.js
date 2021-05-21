74414980134f94bc399621862fd4cd9d8631bb8d
Allehyani-Youssef <yousef4912@gmail.com>
2021-05-21 11:04:52 +0300

    made requestJoke

--- Basic_of_Javascript/PromisesFetchAsync&Await.js
+++ Basic_of_Javascript/PromisesFetchAsync&Await.js
@@ -31,91 +31,101 @@ myPromise.then(value =>{
 
 // fetch 
 
-const url= fetch("https://jsonplaceholder.typicode.com/users")
+// const url= fetch("https://jsonplaceholder.typicode.com/users")
 
-const users = (url).then(response =>{
-    return response.json();
-}).then(data =>{
-    data.forEach(users =>{
-        console.log(users);
-    })
+// const users = (url).then(response =>{
+//     return response.json();
+// }).then(data =>{
+//     data.forEach(users =>{
+//         console.log(users);
+//     })
     
-});
+// });
 
 
-// Async & Await 
-const myUsers = {
-    userList : []
+// // Async & Await 
+// const myUsers = {
+//     userList : []
+// }
+// const asyncfunction = async ()=>{
+//     const response = await url;
+//     const jsonUserData = await response.json();
+//     // console.log(jsonUserData);
+//     return jsonUserData;
+// }
+
+
+// const myUsersFunc = async ()=>{
+//     const data = await asyncfunction();
+//     myUsers.userList = data;
+//     console.log(myUsers.userList);
+// }
+
+// // asyncfunction();
+// myUsersFunc();
+// console.log(myUsers.userList);
+
+
+// // workflow function
+
+// const getAllUserEmail = async ()=>{
+//     const response = await url;
+//     const jsonUserData = await response.json();
+//     const userEmailArray = jsonUserData.map(user =>{
+//         return user.email;
+//     });
+//     postToWebPage(userEmailArray);
+// };
+
+// const postToWebPage = (data)=>{
+//     console.log(data)
+// };
+
+// getAllUserEmail();
+
+// //get
+
+// const getDadJoke = async ()=>{
+//     const response = await fetch("https://icanhazdadjoke.com/", {
+//         method :"GET",
+//         headers: {
+//             Accept: "application/json"
+//         }
+//     });
+//     const jsonJokData = await response.json();
+//     console.log(jsonJokData);
+// };
+
+// getDadJoke();
+
+
+// // post 
+// const jokeObject ={
+//     id: "rzs4T0oWgFd"
+//     , joke: "Two peanuts were walking down the street. One was a salted."
+// }
+
+// const postData = async (jokeObject)=>{
+//     const response = await fetch("https://httpbin.org/anything", {
+//         method :"post",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body : JSON.stringify(jokeObject)
+//     });
+//     const jsonUserData = await response.json();
+//     console.log(jsonUserData.headers);
+// };
+
+// postData(jokeObject);
+
+// 2sd parameter of Fetch
+
+const requestJoke = async(firstName,lastName)=>{
+    const response = await fetch(`https://api.icndb.com/jokes/random?
+    firstName = ${firstName}&lastName=${lastName}`);
+    const jokeRespons =await response.json();
+    console.log(jokeRespons.value.joke);
 }
-const asyncfunction = async ()=>{
-    const response = await url;
-    const jsonUserData = await response.json();
-    // console.log(jsonUserData);
-    return jsonUserData;
-}
 
-
-const myUsersFunc = async ()=>{
-    const data = await asyncfunction();
-    myUsers.userList = data;
-    console.log(myUsers.userList);
-}
-
-// asyncfunction();
-myUsersFunc();
-console.log(myUsers.userList);
-
-
-// workflow function
-
-const getAllUserEmail = async ()=>{
-    const response = await url;
-    const jsonUserData = await response.json();
-    const userEmailArray = jsonUserData.map(user =>{
-        return user.email;
-    });
-    postToWebPage(userEmailArray);
-};
-
-const postToWebPage = (data)=>{
-    console.log(data)
-};
-
-getAllUserEmail();
-
-//get
-
-const getDadJoke = async ()=>{
-    const response = await fetch("https://icanhazdadjoke.com/", {
-        method :"GET",
-        headers: {
-            Accept: "application/json"
-        }
-    });
-    const jsonJokData = await response.json();
-    console.log(jsonJokData);
-};
-
-getDadJoke();
-
-
-// post 
-const jokeObject ={
-    id: "rzs4T0oWgFd"
-    , joke: "Two peanuts were walking down the street. One was a salted."
-}
-
-const postData = async (jokeObject)=>{
-    const response = await fetch("https://httpbin.org/anything", {
-        method :"post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body : JSON.stringify(jokeObject)
-    });
-    const jsonUserData = await response.json();
-    console.log(jsonUserData.headers);
-};
-
-postData(jokeObject);
-
+requestJoke("youssef","Saad");

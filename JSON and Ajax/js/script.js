0dc645097bc46fffc2d40d1049ea58509aeca0cc
Allehyani-Youssef <yousef4912@gmail.com>
2021-06-20 15:17:54 +0300

    make some edit in Json

--- JSON and Ajax/js/script.js
+++ JSON and Ajax/js/script.js
@@ -1,20 +1,31 @@
 let button = document.getElementById("button");
 let info = document.getElementById("info");
-
+let numberOfObjact = 0;
+let Page = 1;
 button.addEventListener("click",function(){
     let xReq = new XMLHttpRequest();
-    xReq.open('GET','js/cars.json');
+    xReq.open('GET','js/cars'+Page+'.json');
     xReq.onload = function(){
        let xData = JSON.parse(xReq.responseText);
-       TheEvent(xData);
+       TheEvent(xData,numberOfObjact);
+       numberOfObjact++;
+       if (numberOfObjact >1){
+        numberOfObjact =0;
+        Page++;
+    };
+    if (numberOfObjact > 1 || Page>3){
+            button.classList.add("hide");
+            button.style.background = "#eee";
+            button.style.color = "lightgray";
+        }
     };
     xReq.send();
 });
 
-let TheEvent = (Data)=>{
-    let DataObjact = "";
+let TheEvent = (Data,number)=>{
+    let DataObjact = [];
     for(let i =0 ; i<Data.length;i++){
-        DataObjact += "<p>"+Data[i].name+"is a "+Data[i].model+"<hr></p>";
-    }
-    info.insertAdjacentHTML("beforeend",DataObjact)
-}
+        DataObjact.push ("<p>"+Data[i].name+"is a "+Data[i].model+"<hr></p>");
+    };
+    info.insertAdjacentHTML("beforeend",DataObjact[number]);
+};
